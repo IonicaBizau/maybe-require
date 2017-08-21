85ff70185127dcb2c761d458f51c8c97a6b9c175
Ionică Bizău <bizauionica@gmail.com>
2017-08-21 19:06:39 +0300

    :arrow_up: 1.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,14 @@
 
 const maybeRequire = require("../lib");
 
-console.log(maybeRequire());
+// foo.js
+//   module.exports = 42
+
+const foo = maybeRequire(`${__dirname}/foo.js`)
+    , bar = maybeRequire(`${__dirname}/bar.js`)
+
+console.log(foo)
+// => 42
+
+console.log(bar)
+// => {}
